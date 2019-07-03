e0a030d2e5bbce0b1c6a89d7c79f35ace0d2b743
O Max Anderson <omaxwellanderson@gmail.com>
2019-07-02 23:43:28 -0500

    God it's ugly but it works

--- index.js
+++ index.js
@@ -100,24 +100,59 @@ f.put('/play', async (req, res) => {
    const albums = body.filter(item => item.type === 'album');
    const tracks = body.filter(item => item.type === 'track');
    const artists = body.filter(item => item.type === 'artist');
-   console.log('playlists', playlists);
-   console.log('albums', albums);
-   console.log('track', tracks);
-   console.log('artists', artists);
 
-   // get the tracks from the relevant playlists
-   const promises = body.map(item => {
-      return axios.get(
-      `https://api.spotify.com/v1/${item.type}s/${item.id}/tracks?access_token=${access_token}`,
-      )
-   });
-   const results = await Promise.all(promises);
+   const playlistPromises = playlists.map(p => axios.get(
+      `https://api.spotify.com/v1/playlists/${p.id}/tracks?access_token=${access_token}`
+   ));
+   const artistPromises = artists.map(a => axios.get(
+      `https://api.spotify.com/v1/artists/${a.id}/top-tracks?access_token=${access_token}&country=US`
+   ));
+   const albumPromises = albums.map(a => axios.get(
+      `https://api.spotify.com/v1/albums/${a.id}/tracks?access_token=${access_token}&limit=50`
+   ));
+
+   const playlistTracks = await Promise.all(playlistPromises);
+   const artistTracks = await Promise.all(artistPromises);
+   const albumTracks = await Promise.all(albumPromises);
+
+   const albumUris = _.flatten(albumTracks.map(({data}) => data.items)).map(t => t.uri);
+   const playlistUris = _.flatten(playlistTracks.map(({data}) => data.items)).map(({track}) => track.uri);
+   const artistUris = _.flatten(artistTracks.map(({data}) => data.tracks)).map(t => t.uri);
+   const trackUris = tracks.map(({uri}) => uri);
+
+   // TODO figure out why this isn't working great
+   const holyLodashMethods = _.compact(_.flatten(_.zip(
+      _.shuffle(albumUris),
+      _.shuffle(playlistUris),
+      _.shuffle(artistUris),
+      _.shuffle(trackUris),
+   )));
+
+   // console.log('a', albumUris);
+   // console.log('a2', artistUris);
+
+   // console.log(dataObjects);
+   // const objects = results.map(result => ({ items: result.data.items, type: result.data.type }));
+   // console.log('objects', objects);
+
+   /*
+   const results = await Promise.all(
+      playlistPromises,
+      artistPromises,
+      albumsPromises
+   );
+   /*
+   console.log('playlistTracks', playlistTracks);
+   console.log('artistTracks', artistTracks);
+   console.log('albumsTracks', albumTracks);
    console.log('results', results);
    // const uris = _.shuffle(_.flatten(results.map(playlist => playlist.data.items.map(obj => (obj.track.uri)))));
    const uris = results.map(playlist => playlist.data.items.map(obj => (obj.track.uri)));
+   console.log('uris', uris);
 
    const holyLodashMethods = _.compact(_.flatten(_.zip(...uris.map(u => _.shuffle(u)))));
 
+*/
    const result = await axios({
       method: 'PUT',
       url: 'https://api.spotify.com/v1/me/player/play',
